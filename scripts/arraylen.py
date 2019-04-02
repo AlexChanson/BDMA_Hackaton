144c59b64d19b8434d1111c1cbe736f6306ac5ee
Alexandre Chanson <a.chanson.tours@gmail.com>
2019-04-02 11:26:46 +0200

    resized images + first csv

--- scripts/arraylen.py
+++ scripts/arraylen.py
@@ -1,11 +1,9 @@
 from PIL import Image
-import pandas as pd
 import numpy as np
 import glob
 
 
 def rowcount(matrix):
-
         pd_im = pd.DataFrame(matrix)
         rowlen = pd_im.shape[0]
         return rowlen
