103047d8040112d205660bd0fa79653e661fcc15
xairon <ringuetnicolas2@gmail.com>
2019-04-02 10:51:07 +0200

    change arraylen

--- scripts/arraylen.py
+++ scripts/arraylen.py
@@ -2,35 +2,25 @@ from PIL import Image
 import pandas as pd
 import numpy as np
 import glob
-rowdes = []
-columndes =[]
-ratiodes =[]
 
-def rowcount(path,rowdes):
-    for filename in glob.glob(path):
-        im=Image.open(filename)
-        pd_im = pd.DataFrame(np.array(im))
-        rowlen = pd_im.shape[0]
-        rowdes.append(rowlen)
-def columncount(path,columndes):
-    for filename in glob.glob(path):
-        im=Image.open(filename)
-        pd_im = pd.DataFrame(np.array(im))
-        columnlen = pd_im.shape[1]
-        columndes.append(columnlen)
-def ratiocount(path,columndes):
-    for filename in glob.glob(path):
-        im=Image.open(filename)
-        pd_im = pd.DataFrame(np.array(im))
-        rowlen = pd_im.shape[0]
-        columnlen = pd_im.shape[1]
-        ratio = rowlen / columnlen
-        ratiodes.append(ratio)
 
-filepath = '/home/xairon/Bureau/BDMA_Hackaton/dataset/0/*.png'
-rowcount(filepath,rowdes)
-print(rowdes)
-columncount(filepath,columndes)
-print(columndes)
-ratiocount(filepath,ratiodes)
-print(ratiodes)
+def rowcount(matrix):
+
+        pd_im = pd.DataFrame(matrix)
+        rowlen = pd_im.shape[0]
+        return rowlen
+
+
+def columncount(matrix):
+    pd_im = pd.DataFrame(matrix)
+    columnlen = pd_im.shape[1]
+    return columnlen
+
+
+def ratiocount(matrix):
+    pd_im = pd.DataFrame(matrix)
+    rowlen = pd_im.shape[0]
+    columnlen = pd_im.shape[1]
+    ratio = rowlen / columnlen
+    return ratio
+
